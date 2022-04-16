077133d2f0eced3a471596ed54585a3aa87220f4
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-16 21:49:36 +0500

    Connect with mongodb altas
    Setup the cors policy and connect backend with mongodb altas with mongoose

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://maqibnaeem2019:Mnaeem12345@memories.r4cdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.Port || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then( () => app.listen(PORT, () => console.log(`Server started at port ${PORT}`)) )
+  .catch( error => console.log(error.message))
+
+// mongoose.set('useFindAndModify', false);
