d4f16b4e704bbc7b9a2b15225cc3c480ddc71364
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-18 16:13:16 +0500

    Added the client side basic structure
    Make the basic structure of the app with material-ui components. Added Posts and Form folders and created styles.js files, which is CSS in JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import React from 'react'
+import Form from './components/Form/Form'
+import Posts from './components/Posts/Posts'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post/Post'
+
+const Posts = () => {
+  return (
+    <React.Fragment>
+      <div>Posts</div>
+      <Post />
+      <Post />
+    </React.Fragment>
+  )
+}
+
+export default Posts
