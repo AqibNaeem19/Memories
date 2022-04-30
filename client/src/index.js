9369d8b7abe2557190829705c0752b9b8f14484c
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-30 11:13:56 +0500

    Added redux template to App
    Install redux dependencies and setup the structural template for redux. This commit belongs to:
    createStore,
    applyMiddleware,
    thunk middleware,
    combineReducers,
    reducers ...

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import reducers from './reducers'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,14 @@
+
+// Initial state is posts here.
+export default ( posts = [], action ) => {
+  switch(action.type){
+    case 'FETCH_ALL':
+      return posts
+
+    case 'CREATE':
+      return posts
+
+    default:
+      return posts
+  }
+}
