03329bc49df4ea5c02a0f17347cdef3055d088ef
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-04-30 12:04:00 +0500

    Added the getPosts action creater
    Implemented all the redux functionality of getting all the posts from the backend server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import memories from './images/memories.png'
 import useStyles from './styles'
 
 const App = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   return (
     <Container maxwidth="lg">
@@ -16,7 +23,7 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Post from './Post/Post'
+import { useSelector } from 'react-redux'
 import useStyles from './styles'
 
 const Posts = () => {
   const classes = useStyles()
+  const posts = useSelector( (state) => state.posts )
+
+  console.log(posts)
   
   return (
     <React.Fragment>
